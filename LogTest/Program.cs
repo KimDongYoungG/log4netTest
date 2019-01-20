887f1a9da32e5d0a4e80b9cbf74fdf061dc365ec
DESKTOP-8N15U0C\DongyoungKim <dykimblue@gmail.com>
2019-01-20 12:37:38 +0900

    <<Init>>

---
+++ LogTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
